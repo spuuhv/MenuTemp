aa794357933bf7540190f656441b6ff381a5b1fa
迷人的卡勒 <isbtsuns@icloud.com>
2025-08-02 12:36:10 +0800

    Initial Commit

---
+++ MenuTemp/MenuTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTempApp.swift
+//  MenuTemp
+//
+//  Created by 孙旗 on 2025/8/2.
+//
+
+import SwiftUI
+
+@main
+struct MenuTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuTempTests/MenuTempTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTempTests.swift
+//  MenuTempTests
+//
+//  Created by 孙旗 on 2025/8/2.
+//
+
+import Testing
+@testable import MenuTemp
+
+struct MenuTempTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
